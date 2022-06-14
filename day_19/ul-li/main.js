a62b6d895bbe9e1ec0c9b5e83a3610e2e2183eea
buihien0109 <buihien01091997@gmail.com>
2022-06-14 10:10:59 +0700

    update

--- day_19/ul-li/main.js
+++ day_19/ul-li/main.js
@@ -31,3 +31,35 @@ addBtn.addEventListener('click', () => {
 })
 
 // 2. Thêm 1 nút remove (tạo bằng Javascript). Chức năng để xóa thẻ li cuối cùng của danh sách ul
+
+
+// Tạo nút remove
+const removeBtn = document.createElement('button')
+removeBtn.textContent = 'Remove'
+document.body.appendChild(removeBtn)
+
+removeBtn.addEventListener('click', () => {
+    let liLast = document.querySelector("#list li:last-child");
+    if (liLast) {
+        liLast.remove();
+    }
+    // document.querySelector("#list li:last-child").remove();
+})
+
+
+// Thêm 1 nút Hide trên đầu của danh sách ul
+
+// Khi bấm vào Hide thì ul sẽ ẩn. Đồng thời label của nút Hide => Show
+// Và ngược lại, khi bấm vào Show thì ul sẽ hiện. Đồng thời label của nút Show => Hide
+
+const hidebtn = document.createElement('button');
+hidebtn.innerText = 'Hide';
+lists.insertAdjacentElement('beforebegin', hidebtn);
+
+hidebtn.addEventListener('click', () => {
+    lists.classList.toggle('hide');
+
+    lists.classList.contains("hide") 
+        ? hidebtn.innerText = 'Show' 
+        : hidebtn.innerText = 'Hide'
+})
